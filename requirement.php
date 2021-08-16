e3a04c4acbb59088b7b2658219ac2b174fd4682b
John Kener <73270248+John-kener@users.noreply.github.com>
2021-08-16 13:45:42 +0530

    Update requirement.php

--- requirement.php
+++ requirement.php
@@ -4,7 +4,6 @@ echo("\e[1;31m\n INSTALLING REQUIREMENTS ...");
 echo ("\033[0m ");
 system("termux-setup-storage");
 system("pkg update && pkg upgrade");
-system("pkg install ruby -y");
 system("pkg install figlet -y");
 system("pkg install python -y");
 system("pip install halo ");
