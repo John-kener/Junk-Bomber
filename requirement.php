d27f7364dbd6d3e7e3bf1a8e7fb4bfa3a7b17fef
John Kener <73270248+John-kener@users.noreply.github.com>
2021-08-16 13:26:17 +0530

    Add files via upload

---
+++ requirement.php
@@ -0,0 +1,17 @@
+<?php
+system("clear");
+echo("\e[1;31m\n INSTALLING REQUIREMENTS ...");
+echo ("\033[0m ");
+system("termux-setup-storage");
+system("pkg update && pkg upgrade");
+system("pkg install ruby -y");
+system("pkg install figlet -y");
+system("pkg install python -y");
+system("pip install halo ");
+system("pkg install nano -y");
+echo (" ");
+echo ("\033[32m INSTALLATION COMPLETED");
+echo ("\033[0m ");
+echo ("\n\n [+]\033[32m Now Run :- \033[94m php Junk-Bomber.php \n\n");
+?>
+
